d8ffaaf6c528c9e8dc8870e9fc1b0e35d8edf754
ahualoh <alex_allie_h@yahoo.com>
2023-01-16 18:10:24 -0500

    Add forEach to Your Table 12.5.2

--- js/app.js
+++ js/app.js
@@ -4,3 +4,23 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 
+// Building Table-build function
+function buildTable(data) {
+    // Clear out any existing data
+    tbody.html("");
+}
+
+// Append Data to table rows
+// Loop through each object in the data and append a row and cells for each value in the row
+data.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+    // Loop through each field in the dataRow and...
+    Object.values(dataRow).forEach((val) => {
+      // add each value as a table cell (td)
+        let cell = row.append("td");
+        cell.text(val);
+        }
+    );
+  });
+
